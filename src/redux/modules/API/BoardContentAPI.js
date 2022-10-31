01a5ba3fa5d2bc45e86542dd9f4517b8f3550aa3
soyoung <0001401@naver.com>
2022-10-31 15:48:38 +0900

    [소영] api 수정

--- src/redux/modules/API/BoardContentAPI.js
+++ src/redux/modules/API/BoardContentAPI.js
@@ -4,5 +4,6 @@ const DATA_URL = "http://13.124.191.202:8080";
 
 //게시글 작성
 export const addBoardContentApi = async (payload) => {
+  console.log("API payload=>", payload);
   await axios.post(`${DATA_URL}/auth/boards/create`, payload);
 };
